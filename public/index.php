7886260c4c2f3c09fc4315ad798b6dddedaa516c
Star88 <yabdyli@gmail.com>
2021-12-18 13:46:43 +0100

    - Added Api route with order endpoint
    - Added an Api route for saving an order.

--- public/index.php
+++ public/index.php
@@ -2,17 +2,22 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Models\Order;
-use App\Models\Product;
 use App\Controllers\OrderController;
 
-$order_input = json_decode(file_get_contents('../example-orders/order1.json'));
 
-$product_inputs = json_decode(file_get_contents('../data/products.json'));
+// Create Router instance
+$router = new \Bramus\Router\Router();
 
-$customer_input = json_decode(file_get_contents('../data/customers.json'));
+// Define routes
+$router->get('/', function() {
+
+	echo "Welcome to OrderDiscount Microservice!";
+
+});
+
+$router->post('/order',  OrderController::saveOrder());
+
+// Run it!
+$router->run();
 
 
-$result = OrderController::makeOrder($order_input, $product_inputs,$customer_input);
-
-print_r($result);
--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 use App\Models\Customer;
 use App\Discounts\DiscountLoyalCustomer;
 use App\Discounts\DiscountProductByCategory;
+use App\Discounts\DiscountCheapestProduct;
 
 class OrderController
 {
@@ -31,8 +32,45 @@ class OrderController
 
 		$DiscountProductByCategory = (new DiscountProductByCategory($customer,2))->discount($newOrder);		
 
-		return $newOrder;
+		return json_encode($newOrder);
 
+	}
+
+	public static function saveOrder()
+	{	
+		if(file_get_contents('php://input')){
+
+			$orderData = json_decode(file_get_contents('php://input'));
+
+			$productData = json_decode(file_get_contents('../data/products.json'));
+
+			$customerData = json_decode(file_get_contents('../data/customers.json'));
+
+			//Create new Order
+			$newOrder = new Order($orderData->{'customer-id'},$orderData->total);
+
+			//Add new products in order and match them with provided json products 
+			$newOrder->insertProducts($orderData, $productData);
+
+			$originalCustomer = array_search($orderData->{'customer-id'}, array_column($customerData,'id'));
+
+			$customer = new Customer(
+							$customerData[$originalCustomer]->id,
+							$customerData[$originalCustomer]->name,
+							$customerData[$originalCustomer]->since,
+							$customerData[$originalCustomer]->revenue
+						);
+
+			$loaylDiscount = (new DiscountLoyalCustomer($customer))->discount($newOrder);
+
+			$DiscountProductByCategory = (new DiscountProductByCategory($customer,2))->discount($newOrder);		
+
+			$DiscountCheapestProduct = (new DiscountCheapestProduct($customer,1))->discount($newOrder);		
+
+			header('Content-Type: application/json'); 
+
+			echo json_encode($newOrder);
+		}
 	}
 
 }
